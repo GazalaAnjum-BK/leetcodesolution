e93527ceaa45a865dc5728ed39598ea62f093a65
GAZALA ANJUM B K <gazalaanjumbk058@gmail.com>
2026-02-16 19:44:23 +0530

    Time: 5 ms (75.7%), Space: 45.8 MB (94.27%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int original=x;
+        int reversed=0;
+        
+        while(x>0){
+        int last=x%10;
+        reversed=reversed*10+last;
+         x=x/10;
+
+        }if(original==reversed){
+            return true;
+        }else return false;
+       
+
+        
+        
+    }
+}
